23a261ab586c3761b13d57d904c6a46f1d1d234a
reynaldsauro <naldsauro@gmail.com>
2021-10-04 10:56:38 -0700

    first commit

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Prelim | Home</title>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+</head>
+<body>
+    <h1>My Laravel</h1>
+    | Home<a href="{{url('/')}}"> About | Contact</a>
+</body>
+</html>
